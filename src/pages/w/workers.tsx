f36f4260c3f638329c715a79c3cdeaa9b2c69e88
devnexushq <nexivon.official@gmail.com>
2026-06-13 10:20:17 +0530

    Add worker-to-worker discovery page

---
+++ src/pages/w/workers.tsx
@@ -0,0 +1,5 @@
+import { PublicWorkersPage } from "@/components/PublicDiscoveryPages";
+
+export default function Page() {
+  return <PublicWorkersPage role="worker" hrefPrefix="/w" title="Workers" />;
+}
