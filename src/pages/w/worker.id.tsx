39c2e864f1b9967dc5084188ba391d26e1d601ad
devnexushq <nexivon.official@gmail.com>
2026-06-13 10:22:01 +0530

    Add worker public worker detail route

---
+++ src/pages/w/worker.id.tsx
@@ -0,0 +1,5 @@
+import { PublicWorkerProfilePage } from "@/components/PublicDiscoveryPages";
+
+export default function Page() {
+  return <PublicWorkerProfilePage role="worker" backTo="/w/workers" />;
+}
